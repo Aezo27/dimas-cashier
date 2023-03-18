9448e939b7b9d666d93e903d963b46af79160e37
Rama Sullivan <stillhopeyou@gmail.com>
2023-03-19 01:48:56 +0700

    update revisi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -137,7 +137,7 @@
 
           </div><!-- End Customers Card -->
 
-          <!-- Reports -->
+          {{-- <!-- Reports -->
           <div class="col-12">
             <div class="card">
 
@@ -154,162 +154,162 @@
                 </ul>
               </div> --}}
 
-              <div class="card-body">
-                <h5 class="card-title">Reports <span>/Today</span></h5>
+          <div class="card-body">
+            <h5 class="card-title">Reports <span>/Today</span></h5>
 
-                <!-- Line Chart -->
-                <div id="reportsChart"></div>
+            <!-- Line Chart -->
+            <div id="reportsChart"></div>
 
-                <script>
-                  document.addEventListener("DOMContentLoaded", () => {
-                    new ApexCharts(document.querySelector("#reportsChart"), {
-                      series: [{
-                        name: 'Sales',
-                        data: [31, 40, 28, 51, 42, 82, 56],
-                      }],
-                      chart: {
-                        height: 350,
-                        type: 'area',
-                        toolbar: {
-                          show: false
-                        },
-                      },
-                      markers: {
-                        size: 4
-                      },
-                      colors: ['#4154f1', '#2eca6a', '#ff771d'],
-                      fill: {
-                        type: "gradient",
-                        gradient: {
-                          shadeIntensity: 1,
-                          opacityFrom: 0.3,
-                          opacityTo: 0.4,
-                          stops: [0, 90, 100]
-                        }
-                      },
-                      dataLabels: {
-                        enabled: false
-                      },
-                      stroke: {
-                        curve: 'smooth',
-                        width: 2
-                      },
-                      xaxis: {
-                        type: 'datetime',
-                        categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-                      },
-                      tooltip: {
-                        x: {
-                          format: 'dd/MM/yy HH:mm'
-                        },
-                      }
-                    }).render();
-                  });
-                </script>
-                <!-- End Line Chart -->
+            <script>
+              document.addEventListener("DOMContentLoaded", () => {
+                new ApexCharts(document.querySelector("#reportsChart"), {
+                  series: [{
+                    name: 'Sales',
+                    data: [31, 40, 28, 51, 42, 82, 56],
+                  }],
+                  chart: {
+                    height: 350,
+                    type: 'area',
+                    toolbar: {
+                      show: false
+                    },
+                  },
+                  markers: {
+                    size: 4
+                  },
+                  colors: ['#4154f1', '#2eca6a', '#ff771d'],
+                  fill: {
+                    type: "gradient",
+                    gradient: {
+                      shadeIntensity: 1,
+                      opacityFrom: 0.3,
+                      opacityTo: 0.4,
+                      stops: [0, 90, 100]
+                    }
+                  },
+                  dataLabels: {
+                    enabled: false
+                  },
+                  stroke: {
+                    curve: 'smooth',
+                    width: 2
+                  },
+                  xaxis: {
+                    type: 'datetime',
+                    categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+                  },
+                  tooltip: {
+                    x: {
+                      format: 'dd/MM/yy HH:mm'
+                    },
+                  }
+                }).render();
+              });
+            </script>
+            <!-- End Line Chart -->
 
-              </div>
-
-            </div>
-          </div><!-- End Reports -->
-
-          <!-- Recent Sales -->
-          <div class="col-12">
-            <div class="card recent-sales">
-
-              {{-- <div class="filter">
-                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                  <li class="dropdown-header text-start">
-                    <h6>Filter</h6>
-                  </li>
-
-                  <li><a class="dropdown-item" href="#">Today</a></li>
-                  <li><a class="dropdown-item" href="#">This Month</a></li>
-                  <li><a class="dropdown-item" href="#">This Year</a></li>
-                </ul>
-              </div> --}}
-
-              <div class="card-body">
-                <h5 class="card-title">Recent Sales <span>| Today</span></h5>
-
-                <table class="table table-borderless datatable">
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">Customer</th>
-                      <th scope="col">Product</th>
-                      <th scope="col">Quantity</th>
-                      <th scope="col">Price</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    @foreach ($today as $tod)
-                      <tr>
-                        <th scope="row"><a href="#">{{ $tod->id }}</a></th>
-                        <td>{{ $tod->nama_pembeli }}</td>
-                        <td>{{ $tod->nama }}</td>
-                        <td>{{ $tod->kuantitas }}</td>
-                        <td>@currency($tod->subtotal)</td>
-                      </tr>
-                    @endforeach
-                  </tbody>
-                </table>
-
-              </div>
-
-            </div>
-          </div><!-- End Recent Sales -->
-
-          <!-- Top Selling -->
-          <div class="col-12">
-            <div class="card top-selling">
-
-              {{-- <div class="filter">
-                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                  <li class="dropdown-header text-start">
-                    <h6>Filter</h6>
-                  </li>
-
-                  <li><a class="dropdown-item" href="#">Today</a></li>
-                  <li><a class="dropdown-item" href="#">This Month</a></li>
-                  <li><a class="dropdown-item" href="#">This Year</a></li>
-                </ul>
-              </div> --}}
-
-              <div class="card-body pb-0">
-                <h5 class="card-title">Top Selling <span>| Today</span></h5>
-                <table class="table table-borderless">
-                  <thead>
-                    <tr>
-                      <th scope="col">Preview</th>
-                      <th scope="col">Product</th>
-                      <th scope="col">Price</th>
-                      <th scope="col">Sold</th>
-                      <th scope="col">Revenue</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    @foreach ($todayGroup as $tod)
-                      <tr>
-                        <th scope="row"><img src="{{ asset('storage/gambar-produk') . '/' . $tod->gambar }}" width="160"></th>
-                        <td>{{ $tod->nama }}</td>
-                        <td>@currency($tod->harga)</td>
-                        <td class="fw-bold">{{ $tod->sumKuantitas }}</td>
-                        <td>@currency($tod->sumTotal)</td>
-                      </tr>
-                    @endforeach
-                  </tbody>
-                </table>
-
-              </div>
-
-            </div>
-          </div><!-- End Top Selling -->
+          </div>
 
         </div>
-      </div><!-- End Left side columns -->
+      </div><!-- End Reports --> --}}
+
+      <!-- Recent Sales -->
+      <div class="col-12">
+        <div class="card recent-sales">
+
+          {{-- <div class="filter">
+                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                  <li class="dropdown-header text-start">
+                    <h6>Filter</h6>
+                  </li>
+
+                  <li><a class="dropdown-item" href="#">Today</a></li>
+                  <li><a class="dropdown-item" href="#">This Month</a></li>
+                  <li><a class="dropdown-item" href="#">This Year</a></li>
+                </ul>
+              </div> --}}
+
+          <div class="card-body">
+            <h5 class="card-title">Recent Sales <span>| Today</span></h5>
+
+            <table class="table table-borderless datatable">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Customer</th>
+                  <th scope="col">Product</th>
+                  <th scope="col">Quantity</th>
+                  <th scope="col">Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($today as $tod)
+                  <tr>
+                    <th scope="row"><a href="#">{{ $tod->id }}</a></th>
+                    <td>{{ $tod->nama_pembeli }}</td>
+                    <td>{{ $tod->nama }}</td>
+                    <td>{{ $tod->kuantitas }}</td>
+                    <td>@currency($tod->subtotal)</td>
+                  </tr>
+                @endforeach
+              </tbody>
+            </table>
+
+          </div>
+
+        </div>
+      </div><!-- End Recent Sales -->
+
+      <!-- Top Selling -->
+      <div class="col-12">
+        <div class="card top-selling">
+
+          {{-- <div class="filter">
+                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                  <li class="dropdown-header text-start">
+                    <h6>Filter</h6>
+                  </li>
+
+                  <li><a class="dropdown-item" href="#">Today</a></li>
+                  <li><a class="dropdown-item" href="#">This Month</a></li>
+                  <li><a class="dropdown-item" href="#">This Year</a></li>
+                </ul>
+              </div> --}}
+
+          <div class="card-body pb-0">
+            <h5 class="card-title">Top Selling <span>| Today</span></h5>
+            <table class="table table-borderless">
+              <thead>
+                <tr>
+                  <th scope="col">Preview</th>
+                  <th scope="col">Product</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Sold</th>
+                  <th scope="col">Revenue</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($todayGroup as $tod)
+                  <tr>
+                    <th scope="row"><img src="{{ asset('storage/gambar-produk') . '/' . $tod->gambar }}" width="160"></th>
+                    <td>{{ $tod->nama }}</td>
+                    <td>@currency($tod->harga)</td>
+                    <td class="fw-bold">{{ $tod->sumKuantitas }}</td>
+                    <td>@currency($tod->sumTotal)</td>
+                  </tr>
+                @endforeach
+              </tbody>
+            </table>
+
+          </div>
+
+        </div>
+      </div><!-- End Top Selling -->
+
+    </div>
+    </div><!-- End Left side columns -->
 
     </div>
   </section>
